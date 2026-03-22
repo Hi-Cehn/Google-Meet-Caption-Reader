5131857418a3f34a4f8f52b706fdd757477e6030
WaffleLover <barontang77@gmail.com>
2026-03-22 20:17:11 +0000

    Added feature to open extension on side panel, and removed unneeded css to simplify for myself

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Set Project default path
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react()
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        background: resolve(__dirname, 'src/background.ts'),
+        sidepanel: resolve(__dirname, 'index.html'),
+      },
+      output: {
+          entryFileNames: '[name].js',
+          assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
 })
